5a1a16a800095dabe455e05bf3b442b70f37e328
Danial Al Ghazali Walangadi <danial.agw.2005@gmail.com>
2024-10-02 10:38:22 +0700

    finished header, added prototype for message-section

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 function App() {
+  let contentMessage =
+    "“YOU HAVE SO MUCH BUT ARE ALWAYS HUNGRY FOR MORE. STOP LOOKING UP AT EVERYTHING YOU DON'T HAVE AND LOOK AROUND AT EVERYTHING YOU DO.“";
+  let contentSender = "Rupi Kaur";
+
   return (
     <>
       <header>
         <div className="left-header">
-          <div>Nav_Button</div>
-          <h1>Picraft</h1>
+          <img src="/nav.png" alt="nav-button" />
+          <img src="/picraft-logo.png" alt="logo" />
         </div>
 
-        <div className="right-header">Encryption_Toggle</div>
+        <img
+          className="right-header"
+          src="toggle-encrypted-on.png"
+          alt="encrypt-on"
+        />
       </header>
 
-      <br />
+      <section className="message-section">
+        <div>
+          <p>Name-Message-Toggle</p>
+        </div>
+        <div className="content-body">
+          <img src="/kurner.png" alt="profile-pic" />
+          <div className="content-text">
+            <p>{contentMessage}</p>
+            <p>–{contentSender}</p>
+          </div>
+        </div>
+        <div>
+          <p>Key-Toggle</p>
+        </div>
+      </section>
+
       <br />
       <br />
       <br />
