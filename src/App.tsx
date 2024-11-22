d5f0f263e4f03d363a3841e8fb9bc478e77f923b
elsa <elsasus808@students.unnes.ac.id>
2024-11-22 21:41:17 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import TrafficAnalyzer from './TrafficCard';
+import './styles/TrafficCard.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="content">
+        <TrafficAnalyzer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
